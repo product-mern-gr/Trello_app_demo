85899cec689dc27cafc2d4c1822bf5f486a72d9b
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2023-02-16 10:23:33 +0700

    setup-router

---
+++ fe/src/Pages/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './main.css'
+
+function Main(props) {
+    return (
+        <div>
+            Main
+        </div>
+    );
+}
+
+export default Main;
---
+++ fe/src/Pages/Registration/Registration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Registration(props) {
+    return (
+        <div>
+            Registration
+        </div>
+    );
+}
+
+export default Registration;
