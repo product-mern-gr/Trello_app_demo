87d299a5d63e6e016a5c4f8e429ba40205a9177c
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2023-02-17 10:11:44 +0700

    setup-router

---
+++ fe/src/page/MainPage/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppBar from '../../components/AppBar/AppBar'
+import BoardBar from "../../components/BoardBar/BoardBar";
+import BoardContent from "../../components/BoardContent/BoardContent";
+
+function MainPage(props) {
+    return (
+        <div className='App'>
+            <AppBar />
+            <BoardBar />
+            <BoardContent />
+        </div>
+    );
+}
+
+export default MainPage;
---
+++ fe/src/page/MenuPage/MenuPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MenuPage(props) {
+    return (
+        <div>
+            MenuPage
+        </div>
+    );
+}
+
+export default MenuPage;
