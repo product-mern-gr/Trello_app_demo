64b5230f5015427dab364c66bbe96e60ba8c5296
“BaPhuoc” <phuoc.18112002@gmail.com>
2023-03-02 15:39:34 +0700

    update loginInputForm and update background login page

--- fe/src/page/LoginPage/LoginPage.js
+++ fe/src/page/LoginPage/LoginPage.js
@@ -6,12 +6,15 @@ import LoginInputForm from '../../components/LoginInputForm/LoginInputForm';
 import './LoginPage.scss'
 import logoHeader from '../../assets/trelloLogo-Login.png'
 import logoFooter from '../../assets/footerLogin.png'
+import backgrLeft from '../../assets/trello-backgr-left.svg'
+import backgrRight from '../../assets/trello-backgr-right.svg'
 
 function LoginPage(props) {
     return (
         <div className='login'>
+            <div className='backgrImg leftImg'><img src={backgrLeft} alt="backgrLeft" /></div>
             <div className='login__wrapper'>
-                <img className='login__logoHeader logo' src={logoHeader}/>
+                <img className='login__logoHeader logo' src={logoHeader} alt="logoHeader"/>
                 <p className='login__title'>Log in to continue</p>
                 <LoginInputForm />
                 <p className='login__subText'>Or</p>
@@ -25,12 +28,13 @@ function LoginPage(props) {
                 </div>
                 <hr />
                 <footer>
-                    <img className='login__logoFooter logo' src={logoFooter}/>
+                    <img className='login__logoFooter logo' src={logoFooter} alt="logoFooter"/>
                     <p>One account for Trello, Jira, Confluence and <a href='google.com'>more.</a></p>
                     <p>Privacy Policy  •  User Notice</p>
                 </footer>
             </div>
 
+            <div className='backgrImg rightImg'><img src={backgrRight} alt="backgrRightt" /></div>
         </div>
     );
 }
